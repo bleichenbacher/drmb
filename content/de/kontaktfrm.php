aa2b843638c0a3d81419278c368adc37f3475f08
zoosky <zoosky@gmail.com>
2013-10-24 09:03:52 +0200

    alternative form

---
+++ content/de/kontaktfrm.php
@@ -0,0 +1,124 @@
+---
+title: Kontakt
+canonical_name: contactfrm
+
+--- 
+	
+<div class="row">
+    <div class="col-sm-6">  
+
+    <h1>Kontakt</h1>
+	Praxis Dr. Bleichenbacher<br>
+	  +41 31 310 24 24<br>
+	  <p>Telefonzeiten Montag bis Freitag von 08:30 bis 15:00 Uhr</p>
+	   <a href="mailto:info@drbleichenbacher.ch">info@drbleichenbacher.ch<br>
+	  <a href="mailto:bleichenbacher@hin.ch">bleichenbacher@hin.ch</a><br>
+
+	  <p>
+	  <a href="http://maps.google.ch/maps?hl=de&sugexp=frgbld&gs_nf=1&cp=19&gs_id=20&xhr=t&q=bern+spitalgasse+36&bav=on.2,or.r_gc.r_pw.r_qf.,cf.osb&biw=1810&bih=950&wrapid=tljp1332387616138036&um=1&ie=UTF-8&ei=IJ9qT7iTNMjR4QT2h43dBQ&sa=X&oi=mode_link&ct=mode&cd=3&sqi=2&ved=0CBIQ_AUoAg" target="_blank">Spitalgasse 36, CH-3011 Bern</a><br> 
+			  
+
+ <p>Karte <a href="http://maps.google.ch/maps?hl=de&sugexp=frgbld&gs_nf=1&cp=19&gs_id=20&xhr=t&q=bern+spitalgasse+36&bav=on.2,or.r_gc.r_pw.r_qf.,cf.osb&biw=1810&bih=950&wrapid=tljp1332387616138036&um=1&ie=UTF-8&ei=IJ9qT7iTNMjR4QT2h43dBQ&sa=X&oi=mode_link&ct=mode&cd=3&sqi=2&ved=0CBIQ_AUoAg" target="_blank">Standort auf Google Maps </a> und  <a href="http://www.sbb.ch" target="_blank">SBB Fahrplan </a>
+  </p>
+
+	</div>
+	
+    <div class="col-sm-6">
+      <img src="/assets/img/0023.jpg" width="350px">
+    </div>
+    
+    </div> <!-- row --> 
+    
+  <div class="row">
+    <div class="col-sm-6">  
+<h3><label>Anfragen und Mitteilungen</label></h3>
+<p>Senden Sie mir einfach eine Mitteilung oder Anfrage &uuml;ber das Kontaktformular, und wir werden uns raschmöglichst melden.
+</p>
+
+<br/>
+<form role="form" class="form-horizontal" action="/de/kontaktfrm/" method="post">
+<!--form role="form" class="form-horizontal" action="/pub_scripts/f2m/f2m.asp" method="post"-->
+<INPUT TYPE="HIDDEN" NAME="F2M_Mail_An" VALUE="info@drbleichenbacher.ch">
+<INPUT TYPE="HIDDEN" NAME="F2M_Sprache_Skript" VALUE="0">
+<INPUT TYPE="HIDDEN" NAME="F2M_Mail_Subject" VALUE="allgemeine Anfrage deutsch">
+<INPUT TYPE="HIDDEN" NAME="F2M_Mail_TextKopf" VALUE="allgemeine Anfrage deutsch:">
+
+<fieldset id="person"><legend>Ansprechpartner</legend>
+
+	<p><label for="name">Name: </label>
+
+    <input class="form-control" id="name" name="name" type="text" size="25"/></p>
+
+    <label for="givenname">Vorname: </label>
+    <input class="form-control" id="name" name="name" type="text" size="25" /></p>
+	<p><label for="birth">Geburtsdatum: </label>
+    <input class="form-control" id="name" name="name" type="text" size="25" /></p>
+	<p><label for="mail">Mailadresse: </label>
+    <input class="form-control" id="mail" name="mail" type="text" size="25" /></p>
+    <p><label for="tel">Telefon: </label>
+    <input class="form-control" id="tel" name="tel" type="text" size="25" /></p>
+    <p><label for="fax">Telefax: </label>
+    <input class="form-control" id="fax" name="fax" type="text" size="25" /></p>
+</fieldset>
+       <!--hier sammeln wir Kontaktdaten-->
+
+<br/>
+<fieldset id="anfrage"><legend>Text</legend>
+    <textarea class="form-control" name ="text" rows="5" cols="30">
+    </textarea>
+<!--Text der Mail-->
+</fieldset>
+<br/>
+
+<fieldset id="antwort"><p><strong>Antwort per</strong></p>
+ <div class="radio">   
+    <label class="checkbox-inline" for="tel2"> 
+    <input type="radio" name="antwort" id="tel2" value="tel" /> Telefon 
+	</label>
+ </div>
+ <div class="radio">        
+    <label class="checkbox-inline" for="fax2">
+    <input  type="radio" name="antwort" id="fax2" value="fax" /> Telefax 
+	</label>
+ </div>
+  <div class="radio">   	
+    <label class="checkbox-inline" for="mail2">
+    <input type="radio" name="antwort" id="mail2" value="mail" /> E-Mail
+     </label>
+ </div>
+
+    <!--Hier erfragen wir die Antwortpräferenz-->
+</fieldset>
+
+<!--fieldset id="absenden"><legend>Mail senden</legend-->
+    
+    <div class="checkbox"> 
+    <label for="confirm"> 
+    <input  type="checkbox" name="confirm" id="confirm" /> 
+    <strong>Ich m&ouml;chte eine Best&auml;tigung der Anfrage per Mail</strong>
+    </label>
+     <!--input class="form-control" type="submit" name="senden" value="senden" /-->
+    </div>
+    <br>
+    <p>
+    <button type="submit" class="btn btn-default">Mail Senden  &raquo;</button>
+    </p>      <!--Sendenbutton-->
+</fieldset>
+
+</form>
+
+
+<br/>
+<p>Für Fragen und Mitteilungen steht Ihnen dieses Kontaktformular zur Verfügung.
+</p>
+<p>Bitte haben Sie Verständnis, dass ich verbindliche Aussagen nur machen kann, wenn Ihre Krankengeschichte bereits bekannt ist.
+</p>
+<p>Die Angaben werden bei uns selbstverständlich vertraulich behandelt,
+es handelt sich aber nicht um eine verschlüsselte Verbindung, die Daten sind unterwegs nicht geschützt.
+</p>
+<p>Bitte geben Sie immer Ihre genaue Adresse an. Anonyme Fragen werden nicht beantwortet.
+</p>
+
+</div>
+</div> <!-- row -->
+
